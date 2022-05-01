1a480f5b6712bc881ab939cca8de29cbf8214032
mahmood seoud <Mahmoodseoud@gmail.com>
2022-05-01 13:47:24 +0200

    leap download plus solution

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,14 @@
+using System;
+
+public static class Leap
+{
+    public static bool IsLeapYear(int year)
+    {
+        if (year%4 == 0 && year%100 != 0 || year%400 == 0){
+            return true; 
+        }
+        else {
+            return false;
+        }
+    }
+}
