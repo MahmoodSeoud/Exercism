7a57c5ca02da592ca22dca2610779fd2d226051d
mahmood seoud <Mahmoodseoud@gmail.com>
2022-05-09 22:01:43 +0200

    Space-Age finished

--- csharp/space-age/SpaceAge.cs
+++ csharp/space-age/SpaceAge.cs
@@ -1,58 +1,45 @@
 using System;
-using System.Globalization;
-
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Utilities;
 
 public class SpaceAge
 {
-    int seconds;
-    NumberFormatInfo setPrecision = new NumberFormatInfo();    
+    private int seconds;
+    private double secondsOnOneEarthYear = 31557600;
 
-    double earthYears = 31556926.0;
+    Dictionary<string, double> planet = new Dictionary<string, double>(){
+        {"Earth", 1 },
+        {"Mercury",0.2408467},
+        {"Venus", 0.61519726},
+        {"Mars", 1.8808158},
+        {"Jupiter", 11.862615},
+        {"Saturn", 29.447498},
+        {"Uranus",84.016846},
+        {"Neptune", 164.79132}
 
-    double mercuryYears = 0.2408467*31556926;
+    };
+
     public SpaceAge(int seconds)
     {
         this.seconds = seconds;
     }
 
-    public double OnEarth()
-    {
-        return seconds/earthYears;
-    }
+    public double OnEarth() => On("Earth");
 
-    public double OnMercury()
-    {
-        setPrecision.NumberDecimalDigits = 2;   
-        return seconds/mercuryYears;
-    }
+    public double OnMercury() => On("Mercury");
 
-    public double OnVenus()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public double OnVenus() => On("Venus");
 
-    public double OnMars()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public double OnMars() => On("Mars");
 
-    public double OnJupiter()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public double OnJupiter() => On("Jupiter");
 
-    public double OnSaturn()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public double OnSaturn() => On("Saturn");
 
-    public double OnUranus()
-    {
-        throw new NotImplementedException("You need to implement this function.");
-    }
+    public double OnUranus() => On("Uranus");
+    public double OnNeptune() => On("Neptune");
 
-    public double OnNeptune()
-    {
-        throw new NotImplementedException("You need to implement this function.");
+    public double On(string plant){
+        return Math.Round(seconds / (planet[plant] * secondsOnOneEarthYear), 2);
     }
 }
