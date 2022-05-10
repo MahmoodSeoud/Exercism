cbb99f5c3d301f4735132046bd1f6f193e8d6f7d
mahmood seoud <Mahmoodseoud@gmail.com>
2022-05-10 14:06:19 +0200

    Difference square still not done

--- csharp/difference-of-squares/DifferenceOfSquares.cs
+++ csharp/difference-of-squares/DifferenceOfSquares.cs
@@ -1,38 +1,14 @@
 using System;
-
+using System.Linq;
 public static class DifferenceOfSquares
 {
-    public static int CalculateSquareOfSum(int max)
-    {
-        double sum = 0.0;
-        
-        for (int i = 0; i < max; i++)
-        {
-            sum = sum + i;
-        }
-        return (int) Math.Pow(sum,2.0);
-       // System.Console.WriteLine(sum);
-        System.Diagnostics.Debug.WriteLine("This is CalculateDifferenceOfSquares:" + CalculateDifferenceOfSquares(max));
-    }
+    public static int CalculateSquareOfSum(int max) => Enumerable.Range(1, max).Sum().Square();
+    public static int CalculateSumOfSquares(int max) => Enumerable.Range(1,max).Sum(i => i.Square());
+    
+    public static int CalculateDifferenceOfSquares(int max) => CalculateDifferenceOfSquares(max) - CalculateSumOfSquares(max);
 
-    public static int CalculateSumOfSquares(int max)
-    {
-        double sum = 0.0;
-
-        for (int i = 0; i < max; i++)
-        {
-            sum = sum + Math.Pow(i,2.0);
-        }
-
-        return (int) sum;
-      //  System.Console.WriteLine(sum);
-      System.Diagnostics.Debug.WriteLine("This is CalculateSumOfSquares:" + CalculateSumOfSquares(max));
-    }
-
-    public static int CalculateDifferenceOfSquares(int max)
-    {
-        System.Diagnostics.Debug.WriteLine("This is CalculateDifferenceOfSquares:" + CalculateDifferenceOfSquares(max));
-        System.Diagnostics.Debug.WriteLine("This is CalculateSumOfSquares:" + CalculateSumOfSquares(max));
-        return CalculateDifferenceOfSquares(max) - CalculateSumOfSquares(max);
-    }
+}
+public static class ExtendIntSquare
+{
+    public static int Square(this int i) => i * i;
 }
