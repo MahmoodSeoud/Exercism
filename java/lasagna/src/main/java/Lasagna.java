5cca31efd4556b095ae452c190e5b275a65577c0
MahmoodSeoud <Mahmoodseoud@gmail.com>
2022-05-23 11:06:29 +0200

    Lasagna exercises passes all tests

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,25 @@
 public class Lasagna {
     // TODO: define the 'expectedMinutesInOven()' method
 
+    public int expectedMinutesInOven() {
+        return 40;
+    }
+
     // TODO: define the 'remainingMinutesInOven()' method
 
+
+    public int remainingMinutesInOven(int minutesInoven) {
+        return this.expectedMinutesInOven() - minutesInoven;
+    }
+    
     // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layers){
+        return 2*layers;
+    }
 
     // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layers, int minutesInoven){
+        return minutesInoven + this.preparationTimeInMinutes(layers);
+
+    }
 }
