072bbfc67b5349692c890a42e9b2a6b6ca632977
MahmoodSeoud <Mahmoodseoud@gmail.com>
2023-09-17 00:29:34 +0200

    two ferr and grains

--- python/grains/grains.py
+++ python/grains/grains.py
@@ -1,15 +1,9 @@
-tempArr = [1,2,4]
-for n in range(3, 64):
-    tempArr.append(2*tempArr[n-1])
-
 def square(number):
-    if number < 1 :
+    if number < 1 or 64 < number:
         raise ValueError("square must be between 1 and 64")
-    if 64 < number :
-        raise ValueError("square must be between 1 and 64")
-    else:
-        return tempArr[number-1]
+    
+    return 2**(number-1)
 
     
-def total():
-    return sum(tempArr)
+def total(number=64):
+    return sum(2**(square-1) for square in range(1, number+1))
