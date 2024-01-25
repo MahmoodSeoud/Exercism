fab109f138401659f66b191c28205374b6ac16ab
Mahmood Mohammed Seoud <mse@peoplenet.dk>
2024-01-25 21:41:30 +0100

    raindrops

---
+++ cpp/raindrops/CMakeFiles/raindrops.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/raindrops.dir/raindrops.cpp.o"
+  "CMakeFiles/raindrops.dir/raindrops.cpp.o.d"
+  "CMakeFiles/raindrops.dir/raindrops_test.cpp.o"
+  "CMakeFiles/raindrops.dir/raindrops_test.cpp.o.d"
+  "CMakeFiles/raindrops.dir/test/tests-main.cpp.o"
+  "CMakeFiles/raindrops.dir/test/tests-main.cpp.o.d"
+  "raindrops"
+  "raindrops.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/raindrops.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/raindrops/raindrops.cpp
@@ -0,0 +1,35 @@
+#include "raindrops.h"
+
+namespace raindrops
+{
+    std::string convert(int number_to_convert)
+    {
+        bool pling = number_to_convert % 3 == 0;
+        bool plang = number_to_convert % 5 == 0;
+        bool plong = number_to_convert % 7 == 0;
+
+        std::string str = "";
+
+        if (pling)
+        {   
+            str += "Pling";
+        }
+
+        if (plang)
+        {
+            str += "Plang";
+        }
+
+        if (plong)
+        {
+            str += "Plong";
+        }
+
+        if (!pling && !plang && !plong)
+        {
+            return std::to_string(number_to_convert);
+        }
+
+        return str;
+    }
+} // namespace raindrops
