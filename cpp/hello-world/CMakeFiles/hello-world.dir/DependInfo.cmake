ee98504f76673dd495790ba69670d87d64541d31
Mahmood Mohammed Seoud <mse@peoplenet.dk>
2024-01-25 16:27:55 +0100

    hello world and lasgna

---
+++ cpp/hello-world/CMakeFiles/hello-world.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/mahmood/Exercism/cpp/hello-world/hello_world.cpp" "CMakeFiles/hello-world.dir/hello_world.cpp.o" "gcc" "CMakeFiles/hello-world.dir/hello_world.cpp.o.d"
+  "/Users/mahmood/Exercism/cpp/hello-world/hello_world_test.cpp" "CMakeFiles/hello-world.dir/hello_world_test.cpp.o" "gcc" "CMakeFiles/hello-world.dir/hello_world_test.cpp.o.d"
+  "/Users/mahmood/Exercism/cpp/hello-world/test/tests-main.cpp" "CMakeFiles/hello-world.dir/test/tests-main.cpp.o" "gcc" "CMakeFiles/hello-world.dir/test/tests-main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
