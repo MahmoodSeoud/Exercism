6abdb0eca4ff05758cff134e268c498674332e38
Mahmood Mohammed Seoud <mse@peoplenet.dk>
2024-01-31 11:36:47 +0100

    armstrong

---
+++ cpp/armstrong-numbers/CMakeFiles/armstrong-numbers.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/armstrong-numbers.dir/armstrong_numbers.cpp.o"
+  "CMakeFiles/armstrong-numbers.dir/armstrong_numbers.cpp.o.d"
+  "CMakeFiles/armstrong-numbers.dir/armstrong_numbers_test.cpp.o"
+  "CMakeFiles/armstrong-numbers.dir/armstrong_numbers_test.cpp.o.d"
+  "CMakeFiles/armstrong-numbers.dir/test/tests-main.cpp.o"
+  "CMakeFiles/armstrong-numbers.dir/test/tests-main.cpp.o.d"
+  "armstrong-numbers"
+  "armstrong-numbers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/armstrong-numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/armstrong-numbers/CMakeFiles/test_armstrong-numbers.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_armstrong-numbers"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/test_armstrong-numbers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
