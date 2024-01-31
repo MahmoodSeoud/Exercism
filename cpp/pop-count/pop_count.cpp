c138d301bbfd485cb073043e63aca6988c9fc956
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-01-31 19:00:29 +0100

    pop_count not done

--- cpp/pop-count/pop_count.cpp
+++ cpp/pop-count/pop_count.cpp
@@ -1,7 +1,27 @@
 #include "pop_count.h"
 
+using namespace std;
 namespace chicken_coop {
 
+    int positions_to_quantity(int num){
+        int decimal, binary = 0, remainder, product = 1;
+        decimal = num;
+        while (decimal != 0) {
+            remainder = decimal % 2;
+            binary += remainder * product;
+            decimal  /= 2;
+            product *= 10;
+        }
 
+        string binary_str = to_string(binary);
+        int count = 0;
+        for (auto it = binary_str.begin(); it < binary_str.end(); ++it) {
+            if (*it == '1') count++;
+        }
+
+
+        cout << "binary:" << binary << endl;
+        return count;
+    }
 
 }  // namespace chicken_coop
--- cpp/pop-count/pop_count.h
+++ cpp/pop-count/pop_count.h
@@ -1,7 +1,8 @@
 #pragma once
+#include <iostream>
 
 namespace chicken_coop {
-    
+    int positions_to_quantity(int num);
 
     
 }  // namespace chicken_coop
