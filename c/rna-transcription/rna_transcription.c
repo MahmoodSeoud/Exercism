78d12342a95507788988e2cce3e1730e5c197b7e
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-17 18:25:27 +0100

    dna rna

---
+++ c/rna-transcription/rna_transcription.c
@@ -0,0 +1,36 @@
+#include "rna_transcription.h"
+#include <stdlib.h>
+#include <string.h>
+
+char *to_rna(const char *dna) {
+  size_t rna_len = strlen(dna) + 1;
+  char *rna = malloc(rna_len);
+  memset(rna, '\0', rna_len);
+
+  for (int i = 0; dna[i]; i++) {
+    switch (dna[i]) {
+    case 'A':
+      rna[i] = 'U';
+      break;
+    case 'C':
+      rna[i] = 'G';
+      break;
+    case 'G':
+      rna[i] = 'C';
+      break;
+    case 'T':
+      rna[i] = 'A';
+      break;
+    default:
+      rna[i] = '\0';
+      break;
+    }
+
+    if (!dna[i]) { // End of dna strand -> jump out of loop
+      free(rna);   // remember to free the allocted mem
+      return NULL;
+    }
+  }
+
+  return rna;
+}
