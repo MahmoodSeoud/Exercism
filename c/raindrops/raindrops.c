b2b7f9b07450d3bd8f5c59383f993db3e230105b
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-18 11:54:49 +0100

    raindrops

---
+++ c/raindrops/raindrops.c
@@ -0,0 +1,14 @@
+#include "raindrops.h"
+#include <stdio.h>
+#include <string.h>
+
+void convert(char result[], int drops) {
+  if (drops % 3 == 0)
+    strcat(result, "Pling");
+  if (drops % 5 == 0)
+    strcat(result, "Plang");
+  if (drops % 7 == 0)
+    strcat(result, "Plong");
+  if (!strcmp(result, ""))
+    sprintf(result, "%d", drops);
+}
