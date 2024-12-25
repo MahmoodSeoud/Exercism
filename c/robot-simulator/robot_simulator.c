37088792ef3fd5656cbd6bbd964a2d6f366e76d7
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-25 19:38:49 +0100

    robots updated

--- c/robot-simulator/robot_simulator.c
+++ c/robot-simulator/robot_simulator.c
@@ -15,18 +15,18 @@ void robot_move(robot_status_t *robot, const char *commands) {
   for (char *c = (char *)commands; *c != '\0'; c++) {
 
     if (*c == 'R')
-      robot->direction = (robot->direction + 1) % 4;
+      robot->direction = (robot->direction + 1) % DIRECTION_MAX;
     else if (*c == 'L')
-      robot->direction = (robot->direction + 3) % 4;
+      robot->direction = (robot->direction + DIRECTION_MAX - 1) % DIRECTION_MAX;
     else if (*c == 'A') {
       if (robot->direction == DIRECTION_NORTH)
-        robot->position.y += 1;
+        robot->position.y++;
       else if (robot->direction == DIRECTION_SOUTH)
-        robot->position.y -= 1;
+        robot->position.y--;
       else if (robot->direction == DIRECTION_EAST)
-        robot->position.x += 1;
+        robot->position.x++;
       else if (robot->direction == DIRECTION_WEST)
-        robot->position.x -= 1;
+        robot->position.x--;
     }
   }
 }
