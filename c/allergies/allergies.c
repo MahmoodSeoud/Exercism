90a9b35a8334278af5b92f9c7b07d07cdbad54b2
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-26 22:12:42 +0100

    init allegens

---
+++ c/allergies/allergies.c
@@ -0,0 +1,5 @@
+#include "allergies.h"
+
+bool is_allergic_to(allergen_t allergen, int score);
+
+allergen_list_t get_allergens(int score);
---
+++ c/allergies/allergies.h
@@ -0,0 +1,26 @@
+#ifndef ALLERGIES_H
+#define ALLERGIES_H
+
+#include <stdbool.h>
+
+typedef enum {
+  ALLERGEN_EGGS = 1,
+  ALLERGEN_PEANUTS = 2,
+  ALLERGEN_SHELLFISH = 4,
+  ALLERGEN_STRAWBERRIES = 8,
+  ALLERGEN_TOMATOES = 16,
+  ALLERGEN_CHOCOLATE = 32,
+  ALLERGEN_POLLEN = 64,
+  ALLERGEN_CATS = 128,
+  ALLERGEN_COUNT = 8,
+} allergen_t;
+
+typedef struct {
+  int count;
+  bool allergens[ALLERGEN_COUNT];
+} allergen_list_t;
+
+bool is_allergic_to(allergen_t allergen, int score);
+allergen_list_t get_allergens(int score);
+
+#endif
