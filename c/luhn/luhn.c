d6f9efaabf1978a6af88bb387022ad4f375ead71
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-26 21:40:30 +0100

    refactored

---
+++ c/luhn/luhn.c
@@ -0,0 +1,35 @@
+#include "luhn.h"
+
+bool luhn(const char *num) {
+
+  int str_len = strlen(num);
+  if (str_len <= 1) {
+    return false;
+  }
+
+  if (num[0] == ' ' && num[1] == '0')
+    return false;
+
+  int sum = 0;
+  bool flag = false;
+
+  for (int i = str_len - 1; i >= 0; i--) {
+    if (num[i] != ' ') {
+      int digit = num[i] - '0';
+      if (!(num[i] >= '0' && num[i] <= '9')) { // If its a not number
+        return false;
+      }
+
+      if (flag) {
+        digit = 2 * digit;
+        if (digit > 9) {
+          digit = digit - 9;
+        }
+      }
+      sum += digit;
+      flag = !flag;
+    }
+  }
+
+  return sum % 10 == 0;
+}
---
+++ c/luhn/luhn.h
@@ -0,0 +1,10 @@
+#ifndef LUHN_H
+#define LUHN_H
+
+#include <ctype.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool luhn(const char *num);
+
+#endif
