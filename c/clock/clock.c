10a80f1e416faa303ef4dfea291bb7184ae3e5f6
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-28 20:14:16 +0100

    clock

---
+++ c/clock/clock.c
@@ -0,0 +1,36 @@
+#include "clock.h"
+#include <stdio.h>
+
+clock_t clock_create(int hour, int minute) {
+
+  clock_t clock;
+  int time_in_miutes =
+      ((hour * MINS_IN_HOUR + minute) % MINS_IN_DAY + MINS_IN_DAY) %
+      MINS_IN_DAY;
+  sprintf(clock.text, "%02d:%02d", time_in_miutes / MINS_IN_HOUR,
+          time_in_miutes % MINS_IN_HOUR);
+  return clock;
+}
+
+clock_t clock_add(clock_t clock, int minute_add) {
+  int hours, mins;
+  sscanf(clock.text, "%02d:%02d", &hours, &mins);
+
+  return clock_create(hours, mins + minute_add);
+}
+
+clock_t clock_subtract(clock_t clock, int minute_subtract) {
+  int hours, mins;
+  sscanf(clock.text, "%02d:%02d", &hours, &mins);
+
+  return clock_create(hours, mins - minute_subtract);
+}
+bool clock_is_equal(clock_t a, clock_t b) {
+
+  int a_hour, a_mins;
+  int b_hour, b_mins;
+  sscanf(a.text, "%02d:%02d", &a_hour, &a_mins);
+  sscanf(b.text, "%02d:%02d", &b_hour, &b_mins);
+
+  return a_hour == b_hour && a_mins == b_mins;
+}
---
+++ c/clock/clock.h
@@ -0,0 +1,20 @@
+#ifndef CLOCK_H
+#define CLOCK_H
+
+#include <stdbool.h>
+#include <stdio.h>
+
+#define MAX_STR_LEN sizeof("##:##")
+#define MINS_IN_HOUR (60)
+#define MINS_IN_DAY (24 * MINS_IN_HOUR)
+
+typedef struct {
+  char text[MAX_STR_LEN];
+} clock_t;
+
+clock_t clock_create(int hour, int minute);
+clock_t clock_add(clock_t clock, int minute_add);
+clock_t clock_subtract(clock_t clock, int minute_subtract);
+bool clock_is_equal(clock_t a, clock_t b);
+
+#endif
