2cf3f575f78a309b86f8a3f23c1f5928fd09ab70
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-28 16:17:13 +0100

    sum of multiples

---
+++ c/sum-of-multiples/sum_of_multiples.h
@@ -0,0 +1,10 @@
+#ifndef SUM_OF_MULTIPLES_H
+#define SUM_OF_MULTIPLES_H
+
+#include <stddef.h>
+#include <stdio.h>
+
+unsigned int sum(const unsigned int *factors, const size_t number_of_factors,
+                 const unsigned int limit);
+
+#endif
