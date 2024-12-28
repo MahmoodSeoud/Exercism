d36a3cfa667a63d84abb29a30d738e649ec8175a
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-28 11:40:46 +0100

    allergies donme

--- c/allergies/allergies.c
+++ c/allergies/allergies.c
@@ -1,5 +1,21 @@
 #include "allergies.h"
 
-bool is_allergic_to(allergen_t allergen, int score);
+bool is_allergic_to(allergen_t allergen, int score) {
+  return ((1 << allergen) & score) > 0;
+}
+allergen_list_t get_allergens(int score) {
 
-allergen_list_t get_allergens(int score);
+  allergen_list_t list;
+  list.count = 0;
+
+  for (allergen_t allergen = ALLERGEN_EGGS; allergen < ALLERGEN_COUNT;
+       allergen++) {
+
+    if (is_allergic_to(allergen, score)) {
+      list.allergens[allergen] = is_allergic_to(allergen, score);
+      list.count++;
+    }
+  }
+
+  return list;
+}
