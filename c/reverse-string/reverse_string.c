f6d11ee43eec437fef81f9f252aa368f4db16f80
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-30 22:48:56 +0100

    reverse string calloc

--- c/reverse-string/reverse_string.c
+++ c/reverse-string/reverse_string.c
@@ -1,11 +1,14 @@
 #include "reverse_string.h"
+#include <stdlib.h>
 
 char *reverse(const char *value) {
+  if (!value)
+    return NULL;
   int len = strlen(value);
-  char *reversed_value = malloc(len + 1);
+  char *reversed_value = calloc(len + 1, sizeof(char));
 
   for (reversed_value += len; *value; value++) {
-    *--reversed_value = *value;
+    *(--reversed_value) = *value;
   }
 
   return reversed_value;
