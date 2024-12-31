95fae8f0585aeab4748d667f0c1f5449b3b39036
MahmoodSeoud <Mahmoodseoud@gmail.com>
2024-12-31 17:16:23 +0100

    bin search in square root

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,10 +1,22 @@
 #include "square_root.h"
+#include <stdint.h>
 
 uint16_t square_root(uint16_t n) {
 
-  for (int i = 1; i <= n; i++) {
-    if (i * i == n)
-      return i;
+  int low = 1;
+  int high = 256;
+
+  uint16_t mid;
+  uint16_t square;
+  while (low <= high) {
+    mid = (low + high) / 2;
+    square = mid * mid;
+    if (square == n)
+      return mid;
+    else if (n < square)
+      high = mid;
+    else
+      low = mid;
   }
 
   return 0;
