8e7be5fa0c06c2bf7021e8b38b90d01ddcaee2be
MahmoodSeoud <Mahmoodseoud@gmail.com>
2025-01-06 16:20:39 +0100

    square root

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -9,11 +9,11 @@ uint16_t square_root(uint16_t n) {
   uint16_t mid;
   uint16_t square;
   while (low <= high) {
-    mid = (low + high) / 2;
+    mid = low + (high - low) / 2;
     square = mid * mid;
     if (square == n)
       return mid;
-    else if (n < square)
+    if (n < square)
       high = mid - 1;
     else
       low = mid + 1;
