a8a05a8421ab01f219eb8086dc42fb86487a0c1a
MahmoodSeoud <Mahmoodseoud@gmail.com>
2025-01-06 16:18:16 +0100

    bin search updated

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -10,10 +10,10 @@ const int *binary_search(int value, const int *arr, size_t length) {
   high = length - 1;
   int mid;
   while (low <= high) {
-    mid = (high + low) / 2;
+    mid = low + (high - low) / 2;
     if (arr[mid] == value)
       return &arr[mid]; /* match found */
-    else if (value < arr[mid])
+    if (value < arr[mid])
       high = mid - 1;
     else
       low = mid + 1;
